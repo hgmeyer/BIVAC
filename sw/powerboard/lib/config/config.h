59a331aa128b45a1c47a2c0ab9b32a110118d8b7
Hanno Gerd Meyer <git@neuromail.de>
2016-10-25 13:35:42 +0200

    Updated preliminary version of powerboard firmware

--- sw/powerboard/lib/config/config.h
+++ sw/powerboard/lib/config/config.h
@@ -1,10 +1,17 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-// Define I2C adress of slave (Note: LSB is the I2C r/w flag and must not be used for addressing!)
-#define SLAVE_ADDR 		0b00001000	// 0x04
-
-// Set sample rate
-#define SAMPLERATE   100 // 10 Hz
-
+// Set CPU frequency
+#ifndef F_CPU
+#define F_CPU 8000000UL   // 8 MHz
 #endif
+
+// Define I2C adress of slave (Note: LSB is the I2C r/w flag and must not be used for addressing!)
+#define SLAVE_ADDR  0b00001000  // 0x04
+
+// Define number of samples to compute moving average from
+#define SAMPLE_N 64
+// Define window size of moving average
+#define SAMPLE_WINDOW 8
+
+#endif  // ifndef _CONFIG_H_
--- sw/powerboard/src/main.c
+++ sw/powerboard/src/main.c
@@ -4,21 +4,95 @@
 #include <config.h>
 #include <usiTwiSlave.h>
 
+void initADC(void)
+{
+        // Set ADMUX register
+        ADMUX =
+                (0 << ADLAR) |  // do not left shift (10bit)
+                (0 << REFS2) |  // set reference voltage to 1.1 V
+                (1 << REFS1) |
+                (0 << REFS0) |
+                (0 << MUX3)  |  // set ADC3 as input
+                (0 << MUX2)  |
+                (1 << MUX1)  |
+                (1 << MUX0);
 
-// Set CPU frequency
-#ifndef F_CPU
-#define F_CPU 8000000UL // 8 MHz
-#endif
+        // Set ADCSRA register
+        ADCSRA =
+                (1 << ADEN)  |  // enable ADC
+                (1 << ADPS2) |  // set prescaler to 64 (125kHz)
+                (1 << ADPS1) |
+                (0 << ADPS0);
+
+        // Wait for ADC register changes to settle
+        _delay_ms(10);
+}
+
+int get_raw_ADC()
+{
+        uint8_t adc_low;
+
+        // Start ADC measurement
+        ADCSRA |= (1 << ADSC);
+
+        // Wait until conversion complete
+        while (ADCSRA & (1 << ADSC)) ;
+
+        // Get low byte of ADC measurement
+        adc_low = ADCL;
+
+        // Return merged low and high byte
+        return (ADCH << 8 | adc_low) * 64; //multiply by 64 to scale up to 16 bit
+}
+
+int get_raw_ADC_moving_average(int sample_n, int sample_window)
+{
+        uint16_t raw_adc_moving_average;
+
+        for(int i=0; i<sample_n; i++)
+        {
+                // Initialize moving average with ADC reading
+                raw_adc_moving_average = get_raw_ADC();
+                // Caluclate integrated moving average
+                raw_adc_moving_average += (get_raw_ADC() - raw_adc_moving_average) / sample_window;
+        }
+
+        return raw_adc_moving_average;
+}
 
 int main(void)
 {
+        uint16_t adc_buffer;
+
+        // Initialize ADC
+        initADC();
+
+        // make the LED pin an output for PORTB4
+        DDRB = 1 << 4;
+
+        // Main loop
         while (1)
         {
-                _delay_ms(SAMPLERATE);
+                // Set ADC3 as single-ended input in ADMUX
+                ADMUX |= (0 << MUX3) | (0 << MUX2) | (1 << MUX1) | (1 << MUX0);
 
-                txbuffer[0] = 1;
-                txbuffer[1] = 2;
-                txbuffer[2] = 3;
+                // Get ADC measurement
+                //adc_buffer = get_raw_ADC();
+                adc_buffer = get_raw_ADC_moving_average(SAMPLE_N, SAMPLE_WINDOW);
+
+                if (adc_buffer > 32768)
+                {
+                        PORTB |= (1 << 4);
+                }
+
+                else
+                {
+                        PORTB &= ~(1 << 4);
+                }
+
+                // Write ADC3 measurement in TWI TX-buffer
+                txbuffer[0] = adc_buffer;
+                txbuffer[1] = adc_buffer >> 8;
         }
 
         return 0;
