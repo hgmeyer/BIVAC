b7c020c18212069f0194cc314f1af7f59feb379a
Hanno Gerd Meyer <git@neuromail.de>
2016-11-22 14:40:33 +0100

    Implemented voltage measurement and switched to ATtiny84 MCU

--- sw/powerboard/src/main.c
+++ sw/powerboard/src/main.c
@@ -1,61 +1,79 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-
-#include <config.h>
 #include <usiTwiSlave.h>
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
+// Define I2C adress of slave (Note: LSB is the I2C r/w flag and must not be used for addressing!)
+#define SLAVE_ADDR  0b00001000  // 0x04
+
+// The number of ADC channels to be multiplexed
+#define ADC_N       4
+
+// TODO: Volatile should not be necessary here. Read: http://www.barrgroup.com/Embedded-Systems/How-To/C-Volatile-Keyword
+// ADC channel and index variables
+volatile uint8_t adc_channels[ADC_N] = {0, 1, 2, 5};
+volatile uint8_t adc_index = 0;
+
+// ADC buffer
+volatile uint16_t adc_buffer;
+volatile uint8_t adc_lowbyte;
 
 void initTimer(void)
 {
         // Set timer to CTC mode
         TCCR0A |= (1 << WGM01);
 
-        // Set prescaler to 1024
-        TCCR0B |= (1 << CS02)|(1 << CS00);
+        // Set prescaler to 64
+        TCCR0B |= (1 << CS01)|(1 << CS00);
 
         // Initialize counter
         TCNT0 = 0;
 
-        // Set compare value corresponding to approx. 100.16Hz
-        OCR0A = 78; // TODO: Multiply sample rate by number of ADCs used.
+        // Set compare value corresponding to 500Hz (100Hz per Channel)
+        OCR0A = 250;
 
         // Enable compare interrupt
-        TIMSK |= (1 << OCIE0A);
+        TIMSK0 |= (1 << OCIE0A);
 }
 
 void initADC(void)
 {
-        // Set ADMUX register
-        ADMUX =
-                (0 << ADLAR) |  // do not left shift (10bit)
-                (0 << REFS2) |  // set reference voltage to 1.1 V
-                (1 << REFS1) |
-                (0 << REFS0) |
-                (0 << MUX3)  |  // set ADC3 as input
-                (0 << MUX2)  |
-                (1 << MUX1)  |
-                (1 << MUX0);
+        // Set reference voltage to 1.1V
+        ADMUX |= (1 << REFS1);
 
-        // Set ADCSRA register
-        ADCSRA =
-                (1 << ADEN)  |  // enable ADC
-                (1 << ADPS2) |  // set prescaler to 64 (125kHz)
-                (1 << ADPS1) |
-                (0 << ADPS0);
+        // Clear MUX0:5 and set adc channel according to current index
+        ADMUX = (ADMUX & ~(0x3F)) | (adc_channels[adc_index] & 0x3F);
+
+        // Enable ADC
+        ADCSRA |= (1 << ADEN);
+
+        // Set prescaler to 64 (125kHz)
+        ADCSRA |= (1 << ADPS2) |
+                  (1 << ADPS1);
 }
 
-ISR (TIMER0_COMPA_vect)
+ISR (TIM0_COMPA_vect)
 {
         // Start ADC measurement
         ADCSRA |= (1 << ADSC);
+
+        if(DEBUG) {
+                // Toggle PB0
+                PORTB ^= (1 << PB0);
+        }
 }
 
 int main(void)
 {
-        uint16_t adc_buffer;
-        uint8_t adc_lowbyte;
 
-        uint8_t adc_n = 0;
+        if(DEBUG) {
+                // Set PB0 and PB1 as output pins
+                DDRB |= (1<<PB0) | (1<<PB1);
+        }
 
         // Calibrate clock
         OSCCAL -= 0;
@@ -75,41 +93,43 @@ int main(void)
         // Enable interrupts
         sei();
 
-        // Main loop
+// Main loop
         while (1)
         {
                 // If ADC measurement has been completed
                 if ((ADCSRA & (1 << ADSC)) == 0)
                 {
+                        if (DEBUG) {
+                                // Toggle PB1
+                                PORTB ^= (1 << PB1);
+                        }
+
                         // Get low byte of ADC measurement
                         adc_lowbyte = ADCL;
 
                         // Scale 10 Bit measurement to 16 Bit
                         adc_buffer = (ADCH << 8 | adc_lowbyte) * 64;
 
-                        // Write current measurement to buffer and switch to next ADC
-                        if (adc_n < 2) {
-                                switch (adc_n) {
-                                // TODO: finish switch statement
-                                case 0:
-                                        txbuffer[0] = adc_buffer;
-                                        txbuffer[1] = adc_buffer >> 8;
-                                        // TODO: switch to correct ADC
-                                        ADMUX |= (0 << MUX3) | (0 << MUX2) | (1 << MUX1) | (1 << MUX0);
-                                        adc_n += 1;
-                                        break;
-                                case 1:
-                                        txbuffer[2] = adc_buffer;
-                                        txbuffer[3] = adc_buffer >> 8;
-                                        // TODO: switch to correct ADC
-                                        ADMUX |= (0 << MUX3) | (0 << MUX2) | (1 << MUX1) | (1 << MUX0);
-                                        adc_n = 0;
-                                        break;
-                                }
+                        // Write ADC measurement to buffer
+                        txbuffer[adc_index+adc_index] = adc_buffer;
+                        txbuffer[adc_index+adc_index+1] = adc_buffer >> 8;
+
+                        // TODO: implement differential current measurement!
+
+                        // Set ADC index
+                        if (adc_index < ADC_N-1) {
+                                adc_index += 1;
+                        }
+                        else{
+                                adc_index = 0;
                         }
 
-                }
+                        // Clear MUX0:5 and set ADC channel
+                        ADMUX = (ADMUX & ~(0x3F)) | (adc_channels[adc_index] & 0x3F);
 
+                        // Wait until next ADC measurement starts
+                        while((ADCSRA & (1 << ADSC)) == 0) {}
+                }
         }
 
         return 0;
