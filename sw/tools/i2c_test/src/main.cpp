f5c7969830a261856d69389b31ead9386e5d964f
Hanno Gerd Meyer <git@neuromail.de>
2016-12-07 12:07:08 +0100

    Added simple Arduino firmware for testing I2C output

---
+++ sw/tools/i2c_test/src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+byte read_address = 4;
+volatile int buffer[10];
+volatile uint16_t value_raw;
+
+void setup() {
+        Serial.begin(9600);
+        Wire.begin();
+        Wire.setClock(400000UL);
+}
+
+void loop() {
+        Wire.beginTransmission(read_address);
+        Wire.endTransmission();
+        Wire.requestFrom(read_address, 10);
+
+        int i = 0;
+        while (Wire.available())
+        {
+                buffer[i] = Wire.read();
+                i++;
+        }
+
+        value_raw = (buffer[0] | buffer[1]<<8);
+        Serial.println(value_raw);
+        delay(100);
+}
