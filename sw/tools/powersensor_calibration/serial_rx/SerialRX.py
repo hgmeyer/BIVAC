a456b98bf07eaded6b934eab2f6126680ed901ec
Hanno Gerd Meyer <git@neuromail.de>
2016-12-19 11:26:54 +0100

    Added script for calibrating PowerSensor board

--- sw/tools/powersensor_calibration/serial_rx/SerialRX.py
+++ sw/tools/powersensor_calibration/serial_rx/SerialRX.py
@@ -13,9 +13,10 @@
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import serial
 import multiprocessing
 import Queue
+
+import serial
 
 
 class SerialRX(multiprocessing.Process):
